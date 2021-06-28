ad749dbfc53205c021e0f490c1f5d8635f3b3ddf
ssuh0o0 <ssuh0o0@daum.net>
2021-06-28 19:08:30 +0900

    21/06/27~28 solved by soo

--- Level2/문자열 압축/soohee.py
+++ Level2/문자열 압축/soohee.py
@@ -1,22 +1,23 @@
 def solution(s):
     answer = [len(s)]
     for num in range(1,len(s)//2+1):
-        length = len(s)
-        cnt = 0
-        isContinue = True
-        slist =list(map(''.join, zip(*[iter(s)]*num)))
-        if slist.count(s[0]) == length:
-            return len(str(length)) +1 
+        cnt = 1
+        string = ''
+        slist = [s[i:i+num] for i in range(0, len(s), num)]
         for l , m in zip(slist[:-1],slist[1:]):
             if l == m:
-                if not isContinue : length += 1
-                length = length - len(l)  
-                isContinue = True
-                cnt += 1  
+                cnt += 1
             else:
-                isContinue = False
-        answer.append(length+len(str(cnt)))
+                string += (str(cnt) + l) if cnt > 1 else l
+                cnt = 1
+        if cnt > 1:
+            string += str(cnt) + l 
+        else:
+            string += m
+        answer.append(len(string))
 
     return min(answer)
 
-print(solution("zzzbbabbabba"))
+print(solution("aabbaccc"))
+# ababyyxxxxcdcdcd
+# 2abyy2xx3cd
