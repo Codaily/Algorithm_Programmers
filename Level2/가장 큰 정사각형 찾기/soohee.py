1e16644eb4d1dd3f32343b68a40ec4178c5f23a1
ssuh0o0 <ssuh0o0@daum.net>
2021-07-04 22:53:13 +0900

    21/07/04 try

--- Level2/가장 큰 정사각형 찾기/soohee.py
+++ Level2/가장 큰 정사각형 찾기/soohee.py
@@ -30,11 +30,26 @@
 #     maxSquare = max(maxSquare1,maxSquare2)
 #     print(maxSquare**2)
 #     return maxSquare**2
+def check(board, idx):
+    for i in range(idx):
+        for j in range(idx):
+            if board[i][j] != board[i+idx][j+idx]:
+                return False
+    return True
 
 def solution(board):
-    cnt = 0
-    print()
-    return 1
+    answer = []
+    for i in range(len(board)):
+        cnt = 0
+        for j in range(len(board)):
+            for idx in range(5-max(i,j)):
+                if check(board, idx):
+                    break
+                else:
+                    cnt += 1
+            if cnt == (5-max(i,j)) **2 :
+                answer.append(cnt)
+    return max(answer)
 
 print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
 # #solution([[0,0,1,1],[1,1,1,1]])
