b07f4eee3de67ba5d6e4da82da2980b8eef83060
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2022-02-03 00:58:50 +0900

    22/02/03 not yet by soo

--- Level3/다단계 칫솔 판매/soohee.py
+++ Level3/다단계 칫솔 판매/soohee.py
@@ -10,26 +10,18 @@ def solution(enroll, referral, seller, amount):
         result[name] = 0
     print(sell) 
 
-    visited = []
-    while seller:
-        n = seller.pop()
-        money = amount.pop() * 100
-        profit = math.ceil(money * 0.9)
-        
-        result[n] += profit
-
     # visited = []
-    # stack = ["-"]
+    # while seller:
+    #     n = seller.pop()
+    #     money = amount.pop() * 100
 
-    # while stack:
-    #     n = stack.pop()
-    #     if n not in visited:
-    #         visited.append(n)
-    #         if n in sell:
-    #             tmp = list(set(sell[n])-set(visited))
-    #             tmp.sort(reverse=True)
-    #             stack += tmp
-    # print(visited)
+    #     for k, v in sell.items():
+    #         if n in v:
+    #             profit = math.ceil(money * 0.9)
+    #             money -= profit
+    #             result[n] += profit
+    #             n = k
+
     return answer
 
 print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"],["young", "john", "tod", "emily", "mary"],[12, 4, 2, 5, 10]))
