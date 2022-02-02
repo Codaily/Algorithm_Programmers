d9888fc866ede3f993e6ff29ba5a74621fb4fcf6
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2022-02-03 01:58:38 +0900

    22/02/03 why it is over time...?

--- Level3/다단계 칫솔 판매/soohee.py
+++ Level3/다단계 칫솔 판매/soohee.py
@@ -8,20 +8,21 @@ def solution(enroll, referral, seller, amount):
         else:
             sell[name] += [enroll[idx]]
         result[name] = 0
-    print(sell) 
+        
+  while seller:
+        n = seller.pop()
+        money = amount.pop() * 100
+        while n != '-':
+            for k, v in sell.items():
+                if n in v:
+                    profit = math.ceil(money * 0.9)
+                    money -= profit
+                    result[n] += profit
+                    n = k
+                    break
+            if money == 0:
+                break
 
-    # visited = []
-    # while seller:
-    #     n = seller.pop()
-    #     money = amount.pop() * 100
-
-    #     for k, v in sell.items():
-    #         if n in v:
-    #             profit = math.ceil(money * 0.9)
-    #             money -= profit
-    #             result[n] += profit
-    #             n = k
-
-    return answer
+    return list(result.values())
 
 print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"],["young", "john", "tod", "emily", "mary"],[12, 4, 2, 5, 10]))
